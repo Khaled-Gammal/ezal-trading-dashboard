36ce0b7b277ac1086fb5b7e8f6213431df47b038
Khaled Gammal <khaledgammal7@gmail.com>
2024-12-01 11:25:18 +0200

    solve error of task

--- src/app/(dashboard)/tasks/page.jsx
+++ src/app/(dashboard)/tasks/page.jsx
@@ -1,11 +1,8 @@
 
 import TasksDataTable from "@/data/tasks/TasksDataTable";
-import { PRODUCTION_URL } from "@/lib/utils";
 
 export default async function TasksPage() {
-  const res=await fetch(`http://localhost:3000/api/employees/`)
-  const data=res.json()
-console.log(data);
+  
   return (
       <div>
        <TasksDataTable />
